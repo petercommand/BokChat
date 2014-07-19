8030a5a7d331e1811be0ad2f516de9f130e44cfd
Petercommand <petercommand@gmail.com>
2014-07-19 18:12:03 +0800

    fix remove node error

--- list.c
+++ list.c
@@ -70,78 +70,58 @@ int add_node_to_channel_list(channel_list** channel_list_ptr, channel_info* chan
   }
   return 0;
 }
-int remove_node_from_channel_list(channel_list** channel_lst, channel_info* channel_inf){
-  while(*channel_lst != NULL){
-    if((*channel_lst)->channel_info == channel_inf){
-      if((*channel_lst)->priv != NULL){
-	if((*channel_lst)->next != NULL){
-	  *channel_lst = (*channel_lst)->priv;
-	  channel_list* temp = (*channel_lst)->next;
-	  (*channel_lst)->next->next->priv = *channel_lst;
-	  (*channel_lst)->next = (*channel_lst)->next->next;
-	  free(temp);
-	  temp = NULL;
-	}
-	else{
-	  *channel_lst = (*channel_lst)->priv;
-	  free((*channel_lst)->next);
-	  (*channel_lst)->next = NULL;
-	}
+int remove_node_from_channel_list(channel_list** channel_list_ptr, channel_info* channel_inf){
+  channel_list* channel_lst = *channel_list_ptr;
+  while(channel_lst != NULL){
+    if(channel_lst->channel_info == channel_inf){
+      if(channel_lst == *channel_list_ptr){
+	free(channel_lst);
+	*channel_list_ptr = NULL;
       }
       else{
-	if((*channel_lst)->next != NULL){
-	  (*channel_lst)->next->priv = NULL;
-	  channel_list* temp = (*channel_lst);
-	  (*channel_lst) = (*channel_lst)->next;
-	  free(temp);
-	  temp = NULL;
+	if(channel_lst->next != NULL){
+	  channel_lst->priv->next = channel_lst->next;
+	  channel_lst->next->priv = channel_lst->priv;
+	  free(channel_lst);
+	  channel_lst = NULL;
 	}
 	else{
-	  free(*channel_lst);
-	  *channel_lst = NULL;
+	  channel_lst->priv->next = NULL;
+	  free(channel_lst);
+	  channel_lst = NULL;
 	}
       }
       break;
     }
-    *channel_lst = (*channel_lst)->next;
+    channel_lst = channel_lst->next;
   }
   return 0;
 }
 
-int remove_node_from_user_list(user_list** user_lst, user_info* user_inf){
-  while(*user_lst != NULL){
-    if((*user_lst)->user_info == user_inf){
-      if((*user_lst)->priv != NULL){
-	if((*user_lst)->next != NULL){
-	  *user_lst = (*user_lst)->priv;
-	  user_list* temp = (*user_lst)->next;
-	  (*user_lst)->next->next->priv = *user_lst;
-	  (*user_lst)->next = (*user_lst)->next->next;
-	  free(temp);
-	  temp = NULL;
-	}
-	else{
-	  *user_lst = (*user_lst)->priv;
-	  free((*user_lst)->next);
-	  (*user_lst)->next = NULL;
-	}
+int remove_node_from_user_list(user_list** user_list_ptr, user_info* user_inf){
+  user_list* user_lst = *user_list_ptr;
+  while(user_lst != NULL){
+    if(user_lst->user_info == user_inf){
+      if(user_lst == *user_list_ptr){
+	free(user_lst);
+	*user_list_ptr = NULL;
       }
       else{
-	if((*user_lst)->next != NULL){
-	  (*user_lst)->next->priv = NULL;
-	  user_list* temp = (*user_lst);
-	  (*user_lst) = (*user_lst)->next;
-	  free(temp);
-	  temp = NULL;
+	if(user_lst->next != NULL){
+	  user_lst->priv->next = user_lst->next;
+	  user_lst->next->priv = user_lst->priv;
+	  free(user_lst);
+	  user_lst = NULL;
 	}
 	else{
-	  free(*user_lst);
-	  *user_lst = NULL;
+	  user_lst->priv->next = NULL;
+	  free(user_lst);
+	  user_lst = NULL;
 	}
       }
       break;
     }
-    *user_lst = (*user_lst)->next;
+    user_lst = user_lst->next;
   }
   return 0;
 }
