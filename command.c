89a889225dfad44a2d5a7919ca3a9f2246134078
Petercommand <petercommand@gmail.com>
2014-08-03 20:10:24 +0800

    fix string size error

--- command.c
+++ command.c
@@ -135,7 +135,7 @@ int process_cmd(user_cmd cmd_info, user_info* user_inf){
       }
       char privmsg_buf[MAX_BUFFER];
       char privmsg_buf2[MAX_BUFFER];
-      snprintf(privmsg_buf, MAX_BUFFER, ":%s!%s@%s PRIVMSG %s :%s", user_inf->user_nick, user_inf->user_nick, user_inf->hostname, target_user->user_nick, irc_args.trailing);
+      snprintf(privmsg_buf, MAX_BUFFER-3, ":%s!%s@%s PRIVMSG %s :%s", user_inf->user_nick, user_inf->user_nick, user_inf->hostname, target_user->user_nick, irc_args.trailing);
       snprintf(privmsg_buf2, MAX_BUFFER, "%s\r\n", privmsg_buf);
       send_message_to_user(target_user, privmsg_buf2);
 
